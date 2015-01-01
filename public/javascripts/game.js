4aae873c01be17566acd57b458fd1be85cd00ae5
hmuar <hmuar34@gmail.com>
2015-01-01 11:31:28 -0500

    smoothing render corrections

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,6 +1,12 @@
 $(function () {
 
   "use strict";
+  Number.prototype.map = function ( in_min , in_max , out_min , out_max ) {
+    var curVal = ( this - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
+    if(curVal < out_min) curVal = out_min;
+    else if(curVal > out_max) curVal = out_max;
+    return curVal;
+  }
 
   // var canvas = document.getElementById('gameCanvas'), ctxCanvas = canvas.getContext('2d'), keyboard = new THREEx.KeyboardState(), ships = preloadShips(), entityImage = new Image();
   // entityImage.src = '../images/entity.png';
@@ -34,10 +40,23 @@ $(function () {
   }
 
   resizeCanvas();
+
+  var prevPlayerStates = {};
+  var doneInit = false;
+  var newX = 0,
+      newY = 0
+  var oldPlayerState = null;
+  var movePercentFactorX = 0.1;
+  var movePercentFactorY = 0.1;
+  var deltaX = 0;
+  var deltaY = 0;
+  var maxDelta = 200;
+  var minDelta = 0.01;
+
   function startGame() {
     GameLoop.start(
       //Render Loop
-      function () {
+      function (delta) {
         ctxCanvas.clearRect(0, 0, canvas.width, canvas.height);
         // ctxCanvas.font = "40pt Helvetica";
         // ctxCanvas.fillText('GarageServer.IO Demo', 10, canvas.height - 50);
@@ -48,11 +67,42 @@ $(function () {
         // entityStates.forEach(function (entity) {
         //   drawRotatedImage(entity.state.ang, entity.state.x, entity.state.y, entityImage);
         // });
-        playerStates.forEach(function (player) {          
-          // drawRotatedImage(player.state.ang, player.state.x, player.state.y, ships[player.state.ship]);
-          drawPlayer(player.state.x, player.state.y, playerImg);
-          // drawPlayer(100, 100);
+
+        playerStates.forEach(function (player) {
+          oldPlayerState = prevPlayerStates[player.id];
+          if(oldPlayerState != undefined){
+            deltaX = player.state.x - oldPlayerState.x;
+            deltaY = player.state.y - oldPlayerState.y;
+            console.log(deltaX);
+            if(Math.abs(deltaX) > minDelta){
+              movePercentFactorX = Math.abs(deltaX).map(0, maxDelta, 0.1, 1.0);
+              newX = oldPlayerState.x + deltaX*movePercentFactorX;
+            }
+            else {
+              console.log("delta not big enough ", deltaX);
+              newX = player.state.x;
+            }
+            if(Math.abs(deltaY) > minDelta) {
+              movePercentFactorY = Math.abs(deltaY).map(0, maxDelta, 0.1, 1.0);
+              newY = oldPlayerState.y + deltaY*movePercentFactorY;
+            }
+            else{
+              newY = player.state.y;
+            }
+          }
+          else{
+            newX = player.state.x;
+            newY = player.state.y;
+            var prevPlayerState = {};
+            prevPlayerState.x = newX;
+            prevPlayerState.y = newY;
+            prevPlayerStates[player.id] = prevPlayerState;
+          }
+          drawPlayer(newX, newY, playerImg);
+          prevPlayerStates[player.id].x = newX;
+          prevPlayerStates[player.id].y = newY;
         });
+
       },
       //Update Loop
       function () {
